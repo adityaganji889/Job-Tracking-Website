bdc0b57caecbec542fd18579279c4d9599d8f13b
Aditya Ganji <88584574+adityaganji889@users.noreply.github.com>
2022-08-02 22:16:45 +0530

    Update Landing.js

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -13,7 +13,7 @@ const Landing = () => {
         {/* info */}
         <div className='info'>
           <h1>
-            job <span>tracking</span> app
+            job <span>tracking</span> website
           </h1>
           <p>
           An online job tracking software for an applicant to keep track of his/her progress during the job search process. Tracks the number of pending, interviewed and declined applications.
